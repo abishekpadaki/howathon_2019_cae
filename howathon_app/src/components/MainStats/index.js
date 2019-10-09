fd8a4ba97f824bf862aaa84c1b4b518fc276479c
Abishek Padaki <abiks192@gmail.com>
2019-10-09 15:28:07 +0530

    Modified MainStats components

--- howathon_app/src/components/MainStats/index.js
+++ howathon_app/src/components/MainStats/index.js
@@ -10,19 +10,23 @@ export default class MainStats extends React.Component{
     render(){
         return(
             
-            
-            <div className="mainstats_card">
-                <Row>
+            <div className="mainstats_div">
+               
+            <div className="mainstats_card flex-container">
+                
 <div className="statl col-sm-6">
-Test
+<h3 className="txtcol">Number Of Boxes</h3>
+<h2 className="numcol">{this.props.numboxes}</h2>
 </div>
 
 <div className="statr col-sm-6">
-Test
+<h3 className="txtcol">Number Of Locations</h3>
+<h2 className="numcol">{this.props.numlocations}</h2>
 </div>
     
-</Row>
+</div>
             </div>
+            
             
            
         )
