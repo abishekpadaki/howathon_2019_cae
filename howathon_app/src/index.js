77a9dbdb03147f5962363dc25aaa1599a5b6f702
Abishek Padaki <abiks192@gmail.com>
2019-10-09 13:37:22 +0530

    Added react-router-dom and chnages to card component

--- howathon_app/src/index.js
+++ howathon_app/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './features/HomePage/homepage';
+import Layout from './features/Layout/index';
 import * as serviceWorker from './serviceWorker';
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/layout" render={()=>{
+        return(<Layout/>);
+        }}
+        />
+        
+      </div>
+    </Router>
+  )
 
-ReactDOM.render(<HomePage/>, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
