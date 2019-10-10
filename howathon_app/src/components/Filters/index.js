d6f5cf3d098c714252fb0272200a64de5482a0a9
Abishek Padaki <abiks192@gmail.com>
2019-10-10 07:50:36 +0530

    Added fix for sc1

--- howathon_app/src/components/Filters/index.js
+++ howathon_app/src/components/Filters/index.js
@@ -23,49 +23,10 @@ const Filters = props => {
     // // const currtime = '12:12'; 
     // var currtime= currdate.getHours() + ":" + currdate.getMinutes() ;
     // console.log(currdate)
-    var today = new Date();
-var currdate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var currtime = "0"+today.getHours() + ":" + today.getMinutes();
-// var currtime='07:25';
-console.log(currdate);
-console.log(currtime);
-    // const prevState = previousState(values);
-    // useEffect(() => {
-    //     if (!compare(prevState, values))
-    //         setValues({
-
-    //         });
-    // });
+   
     let content = <></>
-    if (comp === 'sc1')
-        content = <>
-            <div className="label">Date</div>
-            <Input
-                className="dateInput"
-                type='date'
-               value={currdate}
-            />
-            <div className="label">Time</div>
-            <Input
-                className="dateInput"
-                type='time'
-                value={currtime}
-            />
-            <div className="label">Select Boxes</div>
-            <Select
-                setValues={value => {
-                    setValues({
-                        ...values,
-                        boxes: value
-                    })
-                }}
-                placeholder="Pick some"
-                options={boxes}
-                multiple
-            >
-            </Select>
-        </>
-    else if (comp === 'sc2')
+    
+    if (comp === 'sc1' || comp === 'sc2')
         content = <>
             <div className="label">Date</div>
             <Input
