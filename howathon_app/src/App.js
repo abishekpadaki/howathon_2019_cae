0e1358a1dc8cb9a2abcb3d4a828aa0b8cd8bd13c
Abishek Padaki <abipadak@publicisgroupe.net>
2019-10-17 14:35:33 +0530

    Changed GraphQL url

--- howathon_app/src/App.js
+++ howathon_app/src/App.js
@@ -5,7 +5,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import Router from './router';
 
-const host = 'http://13.126.132.35:2021/graphql';
+const host = '15.206.66.196:2021/graphql ';
 const httpLink = new HttpLink({
   uri: host
 });
